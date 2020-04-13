abfd3be15b47764a71359afe3489bd60af2eb597
Rui Gonçalves <ruippeixotog@gmail.com>
2020-04-13 01:52:27 +0100

    Improve assert error messages

--- src/graph_util.ts
+++ src/graph_util.ts
@@ -3,19 +3,19 @@ import type { NodeSpec } from "./graph";
 
 function assertConnectArity(from: NodeSpec, to: NodeSpec): void {
   assert.equal(to.ins.length, from.outs.length,
-    `Mismatch between in-arity of ${to.toString()} (${to.ins.length})
-      and out-arity of ${from.toString()} (${from.outs.length})`);
+    `Mismatch between in-arity of ${JSON.stringify(to.ins)} (${to.ins.length})
+      and out-arity of ${JSON.stringify(from.outs)} (${from.outs.length})`);
 }
 
 function assertInArity(arity: number, sym: NodeSpec): void {
   assert.equal(arity, sym.ins.length,
-    `Expected in-arity of ${sym.toString()} to be ${arity},
+    `Expected in-arity of ${JSON.stringify(sym.ins)} to be ${arity},
       was ${sym.ins.length} instead`);
 }
 
 function assertOutArity(arity: number, sym: NodeSpec): void {
   assert.equal(arity, sym.outs.length,
-    `Expected out-arity of ${sym.toString()} to be ${arity},
+    `Expected out-arity of ${JSON.stringify(sym.outs)} to be ${arity},
       was ${sym.outs.length} instead`);
 }
 
