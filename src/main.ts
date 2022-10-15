12a132b5f24f4f60847d1a76b3f98296b1947146
Rui Gonçalves <ruippeixotog@gmail.com>
2022-10-16 00:38:38 +0100

    Use yargs to parse CLI arguments

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,59 @@
 import fs from "fs";
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
 
 import parser from "./parser";
 import { importRootDir, loader, runner } from "./runtime";
 import compiler from "./compiler";
 import printer from "./graph_printer";
 
-async function runFile(filename: string): Promise<void> {
-  console.log(`parsing ${filename}...`);
-  const ast = parser.parseFile(filename);
+type Argv = {
+  file: string,
+  dot: boolean | undefined
+}
 
-  console.log(`compiling ${filename}...`);
+async function parseArgs(): Promise<Argv> {
+  const argv = yargs(hideBin(process.argv))
+    .scriptName("sscript")
+    .command("* <file>", "Compile and run a StreamScript file")
+    .positional("file", {
+      describe: "The script file to run",
+      type: "string"
+    })
+    .option("dot", {
+      type: "boolean",
+      describe: "Write dot and png files to the output folder"
+    })
+    .demandOption("file")
+    .help()
+    .alias("help", "h")
+    .strict()
+    .argv;
+
+  return argv.then ? await argv : argv;
+}
+
+async function main(argv: Argv): Promise<void> {
+  console.log(`parsing ${argv.file}...`);
+  const ast = parser.parseFile(argv.file);
+
+  console.log(`compiling ${argv.file}...`);
   const componentStore = await loader.loadComponents();
   const graph = compiler.compileGraph(ast, componentStore, importRootDir);
 
   fs.mkdirSync("out", { recursive: true });
-  fs.writeFileSync("out/graph.dot", printer.toDOT(graph));
-  fs.writeFileSync("out/graph_full.dot", printer.toDOT(graph, true));
-  printer.toPNG(graph, "out/graph.png");
-  printer.toPNG(graph, "out/graph_full.png", true);
+  if (argv.dot) {
+    fs.writeFileSync("out/graph.dot", printer.toDOT(graph));
+    fs.writeFileSync("out/graph_full.dot", printer.toDOT(graph, true));
+    printer.toPNG(graph, "out/graph.png");
+    printer.toPNG(graph, "out/graph_full.png", true);
+  }
 
-  console.log(`running ${filename}...`);
+  console.log(`running ${argv.file}...`);
   await runner.runGraph(graph, componentStore);
 }
 
-if (process.argv.length > 2) {
-  const filename = process.argv[2];
-  runFile(filename)
-    .then(() => console.log("Finished."))
-    .catch(err => console.error("ERROR:", err));
-}
+parseArgs()
+  .then(main)
+  .then(() => console.log("Finished."))
+  .catch(err => console.error("ERROR:", err));
